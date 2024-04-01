931f7932be8bc1e10dc3802cd4af0461ac092574
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2024-04-01 17:57:16 +0300

    [Enh](Backend): Made settings and views work with my paths.

--- src/backend/settings.py
+++ src/backend/settings.py
@@ -125,3 +125,4 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'media')
+print(MEDIA_ROOT, MEDIA_URL)
--- src/backend/views.py
+++ src/backend/views.py
@@ -4,7 +4,9 @@ from django.core.files.storage import FileSystemStorage
 from django.shortcuts import render
 from PIL import Image
 
-imageClassList = {"0": "Man", "1": "Wooman", "2": "Unknown"}  # Сюда указать классы
+from backend.settings import BASE_DIR
+
+imageClassList = {"0": "Man", "1": "Woman", "2": "Unknown"}  # Сюда указать классы
 
 
 def scoreImagePage(request):
@@ -17,17 +19,19 @@ def predictImage(request):
     filePathName = fs.save("images/" + fileObj.name, fileObj)
     filePathName = fs.url(filePathName)
     modelName = request.POST.get("modelName")
-    scorePrediction = predictImageData(modelName, "." + filePathName)
+    scorePrediction = predictImageData(modelName, "./src" + filePathName)
     context = {"scorePrediction": scorePrediction}
 
     return render(request, "scorepage.html", context)
 
 
 def predictImageData(modelName, filePath):
+    print(filePath)
+    print(BASE_DIR)
     img = Image.open(filePath).convert("RGB")
-    img = np.asarray(img.resize((32, 32), Image.ANTIALIAS))
+    img = np.asarray(img.resize((32, 32), Image.LANCZOS))
     sess = onnxruntime.InferenceSession(
-        r"C:\DZ1\media\models\cifar100.onnx"
+        r"/Users/aspakalo/Projects/--educational/t5-2-/Neuroscience__/Homework1/src/resnet_avatars_gender/models/resnet_avatars_gender.onnx"
     )  # <-Здесь требуется указать свой путь к модели
     outputOFModel = np.argmax(
         sess.run(None, {"input": np.asarray([img]).astype(np.float32)})
